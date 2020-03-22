1aa91aafc361ea0b8a83bd6448a423fc1a389e25
Valentina Tadic <valentina.tadic008@gmail.com>
2020-03-22 19:55:04 +0100

    ponovni pokusaj

---
+++ Nedelja8/cetvrtak_zadatak/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StoriesContainer } from './containers/StoryContainer';
+const App = () => {
+return(<StoriesContainer></StoriesContainer>)
+}
+export default App;
